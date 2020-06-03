e01a4141ebe5f8796af4a9e91a35e559517f1f7e
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-03 00:31:20 -0400

    Add title to appear in question and random choice

--- questions.js
+++ questions.js
@@ -1,14 +1,24 @@
 var startButton = document.getElementById('start-btn')
 startButton.addEventListener('click', startGame)
 var questionContainerElements = document.getElementById('question-container')
+var shuffledQuestions, currentQuestionIndex
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
 function startGame() {
   startButton.classList.add('hide')
   questionContainerElements.classList.remove('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  setNextQuestion()
 }
 
 function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(title) {
+  questionElement.innerText = (title.title)
 }
 
 function selectAnswer() {
