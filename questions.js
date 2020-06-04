d908fea919d51ea104a1cbda0609b0c88c1fb9b5
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-03 23:20:00 -0400

    Add 3 more questions

--- questions.js
+++ questions.js
@@ -2,7 +2,8 @@ var startButton = document.getElementById('start-btn')
 startButton.addEventListener('click', startGame)
 
 var questionContainerElements = document.getElementById('question-container')
-var shuffledQuestions, currentQuestionIndex
+var shuffledQuestions
+var currentQuestionIndex
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
 
@@ -81,5 +82,29 @@ var questions = [
         {text: "curly brackets", correct: false},
         {text: "parentheses", correct: true},
         {text: "nsquare brackets", correct: false},]
-    }
+    },
+    {
+      title: "Which of the following will write Hello World in an alert box?",
+      choices: [
+        {text: "msg('Hello World')", correct: false}, 
+        {text: "alertBox('Hello World')", correct: false},
+        {text: "alert('Hello World')", correct: true},
+        {text: "msgBox('Hello World')", correct: false},]
+    },
+    {
+      title: "How to call a function named myfunction",
+      choices: [
+        {text: "call myfunction", correct: false}, 
+        {text: "myfunction call", correct: false},
+        {text: "myfunction()", correct: true},
+        {text: "()myfunction", correct: false},]
+    },
+    {
+      title: "What does this line mean 'while (exitNow == true)'?",
+      choices: [
+        {text: "No meaning", correct: false}, 
+        {text: "exit while when exitNow is false", correct: false},
+        {text: "exit while when exitNow is true", correct: true},
+        {text: "All Above", correct: false},]
+    },
   ];
